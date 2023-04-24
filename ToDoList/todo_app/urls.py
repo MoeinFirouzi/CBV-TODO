650951c0a409d90fa64c152a0da7fef53a281f5f
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-04-24 16:03:44 +0330

    Create TaskListView in todo app

--- ToDoList/todo_app/urls.py
+++ ToDoList/todo_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todo_app.views import IndexView
+from todo_app.views import IndexView, TaskListView
 
 app_name = "todo"
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('tasks/', TaskListView.as_view(), name="tasks"),
 ]
--- ToDoList/todo_app/views.py
+++ ToDoList/todo_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from todo_app.models import Task
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -8,3 +8,26 @@ class IndexView(LoginRequiredMixin, TemplateView):
     A simple view for testing
     """
     template_name = "todo_app/index.html"
+
+
+class TaskListView(ListView):
+    """
+    This class represents a view that displays a list of tasks owned
+    by the current user.
+
+    Attributes:
+        template_name (str): The name of the HTML template used to
+        render the view.
+        context_object_name (str): The name of the variable used to
+        store the list of tasks in the context.
+
+    Methods:
+        get_queryset(): Returns a queryset of tasks owned by the
+        current user.
+    """
+    template_name = "todo_app/tasks_list.html"
+    context_object_name = "task_list"
+
+    def get_queryset(self):
+        query_set = Task.objects.filter(owner=self.request.user.id)
+        return query_set
